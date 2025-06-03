db404e64c186aa759f4130d8cd9672289deeab85
Igor <klim.iv@gmail.com>
2025-06-03 14:39:45 +0300

    create instance  for  Linux

--- webview_flutter_linux/lib/src/webview_flutter_linux.dart
+++ webview_flutter_linux/lib/src/webview_flutter_linux.dart
@@ -3,17 +3,16 @@ import 'package:flutter/services.dart';
 import 'package:webview_flutter_platform_interface/webview_flutter_platform_interface.dart';
 
 /// The Linux implementation of [WebviewFlutterPlatform].
-class WebviewFlutterLinux extends WebviewFlutterPlatform {
+class WebviewFlutterLinux extends WebViewPlatform {
   /// The method channel used to interact with the native platform.
   @visibleForTesting
   final methodChannel = const MethodChannel('webview_flutter_linux');
 
   /// Registers this class as the default instance of [WebviewFlutterPlatform]
   static void registerWith() {
-    WebviewFlutterPlatform.instance = WebviewFlutterLinux();
+    WebViewPlatform.instance = WebviewFlutterLinux();
   }
 
-  @override
   Future<String?> getPlatformName() {
     return methodChannel.invokeMethod<String>('getPlatformName');
   }
